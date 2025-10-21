1eff87a479199d9371f9323f7bd902d830e03ea3
Lakshmikanth Reddy Byrapuram <75545674+IRONMAN3442@users.noreply.github.com>
2025-10-21 16:30:50 +0530

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,42 @@
+
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  reactStrictMode: true,
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  experimental: {
+    allowedDevOrigins: [
+        'https://6000-firebase-studio-1759866139411.cluster-qxqlf3vb3nbf2r42l5qfoebdry.cloudworkstations.dev',
+    ]
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
